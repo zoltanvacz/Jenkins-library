830461f2d034f0739113b2e79f8b1cb6b280132b
Vacz <zoltan.vacz@sap.com>
2024-02-20 14:04:20 +0100

    bugfix

--- Jobs/RELEASE.groovy
+++ Jobs/RELEASE.groovy
@@ -1,53 +1,14 @@
-def getAgents() {
-    def agents = []
-    def nodes = Jenkins.instance.nodes
-    for (def i = 0; i < 5; i++) {
-        agents.add("Agent" + i)
-    }
-    return agents
-}
-
 pipeline {
     agent any
+    parameters {
+        choice(name: 'Application', choices: ['Devops-Test-App'], description: 'Select application')
+        string(name: 'VERSION', description: 'Enter version')
+    }
     environment {
         DOCKER_CREDS = credentials('docker')
         GITHUB_TOKEN = credentials('GitHubToken')
     }
     stages {
-        stage('Jenkins UI') {
-            steps {
-                script {
-                    properties { [
-                        parameters([
-                             [$class: 'ChoiceParameter',
-                                        choiceType: 'PT_MULTI_SELECT',
-                                        filterLength: 1,
-                                        filterable: true,
-                                        name: 'SVCNAME',
-                                        script: [
-                                            $class: 'GroovyScript',
-                                            fallbackScript: [
-                                                classpath: [],
-                                                sandbox: true,
-                                                script:
-                                                    "return['Could not fetch the services']"
-                                            ],
-                                            script: [
-                                                classpath: [],
-                                                sandbox: true,
-                                                script: 'return ' + getAgents()
-                                            ]
-                                        ]
-                                    ],
-                        choice(name: 'Application', choices: ['Devops-Test-App'], description: 'Select application'),
-                        string(name: 'VERSION', description: 'Enter version'),
-                        choice(name: 'Agent', choices: getAgents(), description: 'Select agent')
-                        ])
-                    ] }
-
-                    }
-                }
-            }
         stage('Clone App Repo') {
             steps {
                 script {
